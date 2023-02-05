2f1af29d6af61a9e76d156e89afc17e7d2bb2a8a
danimatanovic <danijeladacamat@gmail.com>
2023-02-05 22:45:33 +0100

    TIC TAC TOE done

--- first/script.js
+++ first/script.js
@@ -21,7 +21,7 @@ const winningConditions = [
     [2, 4, 6]
 ];
 
-function handleCellPlayed(clickedCell, clickedCellIndex) {
+function cellPlayed(clickedCell, clickedCellIndex) {
     gameState[clickedCellIndex] = currPlayer;
     clickedCell.innerHTML = currPlayer;
 }
@@ -30,3 +30,61 @@ function playerChange() {
     currPlayer = currPlayer === "X" ? "O" : "X";
     displayGameStatus.innerHTML = currPlayerTurn();
 }
+
+function resultValidation() {
+    let roundWon = false;
+    for (let i = 0; i <= 7; i++) {
+        const winCondition = winningConditions[i];
+        let a = gameState[winCondition[0]];
+        let b = gameState[winCondition[1]];
+        let c = gameState[winCondition[2]];
+        if (a === '' || b === '' || c === '') {
+            continue;
+        }
+        if (a === b && b === c) {
+            roundWon = true;
+            break
+        }
+    }
+
+    if (roundWon) {
+        gameActive = false;
+        setTimeout(restartGame,3000);
+        displayGameStatus.innerHTML = winMessage();
+        return;
+    }
+
+    let roundDraw = !gameState.includes("");
+    if (roundDraw) {
+        gameActive = false;
+        setTimeout(restartGame,3000);
+        displayGameStatus.innerHTML = drawMessage();
+        return;
+    }
+
+    playerChange();
+}
+
+function cellClick(clickedCellEvent) {
+    const clickedCell = clickedCellEvent.target;
+    const clickedCellIndex = parseInt(clickedCell.getAttribute('data-cell-index'));
+
+    if (gameState[clickedCellIndex] !== "" || !gameActive) {
+        return;
+    }
+
+    cellPlayed(clickedCell, clickedCellIndex);
+    resultValidation();
+}
+
+function restartGame() {
+    gameActive = true;
+    currPlayer = "X";
+    gameState = ["", "", "", "", "", "", "", "", ""];
+    displayGameStatus.innerHTML = currPlayerTurn();
+    document.querySelectorAll('.cell').forEach(cell => cell.innerHTML = "");
+}
+
+
+document.querySelectorAll('.cell').forEach(cell => cell.addEventListener('click', cellClick));
+
