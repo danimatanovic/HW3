546cf23e6bb4cc862e854a048f01d39986cb99b7
danimatanovic <danijeladacamat@gmail.com>
2023-02-07 22:20:28 +0100

    palindrome

--- second/script.js
+++ second/script.js
@@ -1,6 +1,7 @@
 var numberOfBoxes = document.getElementById('numberOfBoxes');
+var lettersForPalindrome="";
 function myFunction() {
-  var numOfChar = parseInt(document.getElementById('numberOfChar').value);
+  const numOfChar = parseInt(document.getElementById('numberOfChar').value);
   for (let i = 0; i < numOfChar; i++) {
     var square = document.createElement("input");
     square.style.width = "40px";
@@ -13,8 +14,15 @@ function myFunction() {
     numberOfBoxes.appendChild(square);
     let inputElement = document.getElementById("squar" + i);
     inputElement.addEventListener("input", function () {
-      if (inputElement.value.length > 1) {
-        inputElement.value = inputElement.value.slice(0, 1);
+      if (isCharacterALetter(inputElement.value) == false) {
+        inputElement.value = "";
+      }
+      else {
+        if (inputElement.value.length > 1) {
+          inputElement.value = inputElement.value.slice(0, 1);
+        }
+        lettersForPalindrome=lettersForPalindrome+inputElement.value;
+        checkPalindrome(lettersForPalindrome);
       }
     });
   }
@@ -24,8 +32,7 @@ function myFunction() {
 
 }
 function addNumber() {
-  let i = numberOfBoxes.childNodes.length -3;
-  console.log(numberOfBoxes.childNodes);
+  let i = numberOfBoxes.childNodes.length - 3;
   var square = document.createElement("input");
   square.style.width = "40px";
   square.style.height = "40px";
@@ -36,34 +43,43 @@ function addNumber() {
   numberOfBoxes.appendChild(square);
   let inputElement = document.getElementById("squar" + i);
   inputElement.addEventListener("input", function () {
-    if (inputElement.value.length > 1) {
-      inputElement.value = inputElement.value.slice(0, 1);
+    if (isCharacterALetter(inputElement.value) == false) {
+      inputElement.value = "";
+    }
+    else {
+      if (inputElement.value.length > 1) {
+        inputElement.value = inputElement.value.slice(0, 1);
+      }
+      lettersForPalindrome=lettersForPalindrome+inputElement.value;
+      checkPalindrome(lettersForPalindrome);
     }
   });
   let removeX = document.getElementById('removeX');
-    var buttonX = document.createElement("button");
-    buttonX.innerHTML = "X";
-    buttonX.style.backgroundColor = "red";
-    buttonX.style.color = "white";
-    buttonX.style.borderRadius = "50%";
-    buttonX.style.fontWeight = "bold";
-    buttonX.style.border = "red";
-    buttonX.style.fontSize = "20px";
-    buttonX.style.height = "35px";
-    buttonX.style.width = "35px";
-    buttonX.style.margin=" 0 16px";
-    buttonX.style.cursor="pointer";
-    buttonX.id="button"+i;
-    removeX.appendChild(buttonX);
-    buttonX.addEventListener("click", function () {
-      var lastChild= removeX.lastElementChild;
-      removeX.removeChild(lastChild);
-    var index=i;
-      removeSquare(index);
-    });
-  }
+  var buttonX = document.createElement("button");
+  buttonX.innerHTML = "X";
+  buttonX.style.backgroundColor = "red";
+  buttonX.style.color = "white";
+  buttonX.style.borderRadius = "50%";
+  buttonX.style.fontWeight = "bold";
+  buttonX.style.border = "red";
+  buttonX.style.fontSize = "20px";
+  buttonX.style.height = "35px";
+  buttonX.style.width = "35px";
+  buttonX.style.margin = " 0 16px";
+  buttonX.style.cursor = "pointer";
+  buttonX.id = "button" + i;
+  removeX.appendChild(buttonX);
+  buttonX.addEventListener("click", function () {
+    var lastChild = removeX.lastElementChild;
+    removeX.removeChild(lastChild);
+    var index = i;
+    removeSquare(index);
+    lettersForPalindrome=lettersForPalindrome.substring(0,i) + lettersForPalindrome.substring(i+1); 
+    checkPalindrome(lettersForPalindrome);
+  });
+}
 
-function createX(n){
+function createX(n) {
   let removeX = document.getElementById('removeX');
   for (let j = 0; j < n; j++) {
     var buttonX = document.createElement("button");
@@ -76,24 +92,39 @@ function createX(n){
     buttonX.style.fontSize = "20px";
     buttonX.style.height = "35px";
     buttonX.style.width = "35px";
-    buttonX.style.margin=" 0 16px";
-    buttonX.style.cursor="pointer";
-    buttonX.id="button"+j;
+    buttonX.style.margin = " 0 16px";
+    buttonX.style.cursor = "pointer";
+    buttonX.id = "button" + j;
     removeX.appendChild(buttonX);
     buttonX.addEventListener("click", function () {
-      var lastChild= removeX.lastElementChild;
+      var lastChild = removeX.lastElementChild;
       removeX.removeChild(lastChild);
-    var index=j;
+      var index = j;
       removeSquare(index);
+      lettersForPalindrome=lettersForPalindrome.substring(0,j) + lettersForPalindrome.substring(j+1); 
+      checkPalindrome(lettersForPalindrome);
     });
   }
- 
+
 }
-function removeSquare(m){
-    var specificChild = numberOfBoxes.childNodes[m+3];
-    numberOfBoxes.removeChild(specificChild);
-    if(numberOfBoxes.childNodes.length===3){
-      const button = document.getElementById("addChar");
-      button.classList.remove("show");
-    }
+function removeSquare(m) {
+  var specificChild = numberOfBoxes.childNodes[m + 3];
+  numberOfBoxes.removeChild(specificChild);
+  if (numberOfBoxes.childNodes.length === 3) {
+    const button = document.getElementById("addChar");
+    button.classList.remove("show");
+  }
+}
+function isCharacterALetter(char) {
+  return (/^[a-zA-Z\s]+$/).test(char)
+}
+
+function checkPalindrome(string) {
+  let len = string.length;
+  for (let i = 0; i < len / 2; i++) {
+      if (string[i] !== string[len - 1 - i]) {
+       return document.getElementById("palindrome").innerHTML='It is not a palindrome';
+      }
+  }
+ return document.getElementById("palindrome").innerHTML ="IT IS A PALINDROME";
 }
