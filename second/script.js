fecb87144b90f73732a09f040db5c35701d1cc17
danimatanovic <danijeladacamat@gmail.com>
2023-02-07 01:38:37 +0100

    add char

--- second/script.js
+++ second/script.js
@@ -1,24 +1,41 @@
 function myFunction() {
-var numOfChar = parseInt(document.getElementById('numberOfChar').value);
-let numberOfBoxes = document.getElementById('numberOfBoxes');
-for (let i = 0; i < numOfChar; i++) {
+  var numOfChar = parseInt(document.getElementById('numberOfChar').value);
+  let numberOfBoxes = document.getElementById('numberOfBoxes');
+  for (let i = 0; i < numOfChar; i++) {
     var square = document.createElement("input");
     square.style.width = "40px";
     square.style.height = "40px";
     square.style.margin = "0 10px"
     square.style.border = "2px solid black";
-    square.style.textAlign ="center";
-    square.id="squar"+i;
+    square.style.textAlign = "center";
+    square.id = "squar" + i;
     numberOfBoxes.appendChild(square);
+    let inputElement = document.getElementById("squar" + i);
+    inputElement.addEventListener("input", function () {
+      if (inputElement.value.length > 1) {
+        inputElement.value = inputElement.value.slice(0, 1);
+      }
+    });
+  }
+  const button = document.getElementById("add");
+  button.classList.add("show");
 }
-for(let j = 0; j < numOfChar; j++){
-    let inputElement = document.getElementById("squar"+j);
-    console.log(inputElement);
-    inputElement.addEventListener("input", function() {
+function addNumber() {
+  let numberOfBoxes = document.getElementById('numberOfBoxes');
+  let i = numberOfBoxes.childNodes.length - 2;
+  var square = document.createElement("input");
+  square.style.width = "40px";
+  square.style.height = "40px";
+  square.style.margin = "0 10px"
+  square.style.border = "2px solid black";
+  square.style.textAlign = "center";
+  square.id = "squar" + i;
+  numberOfBoxes.appendChild(square);
+  let inputElement = document.getElementById("squar" + i);
+  inputElement.addEventListener("input", function () {
     if (inputElement.value.length > 1) {
       inputElement.value = inputElement.value.slice(0, 1);
     }
   });
-}
 }
 
